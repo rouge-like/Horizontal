2d731b168bb0ee4dbca9979c30d6d6061dcd8369
Kim Haechan <khc534854@gmail.com>
2025-10-31 14:17:38 +0900

    20251031 헤더 수정

--- Source/Horizontal/Private/Khc/Gimmick/MainGameState.cpp
+++ Source/Horizontal/Private/Khc/Gimmick/MainGameState.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "Net/UnrealNetwork.h"
 #include "Khc/Gimmick/MainGameState.h"
+#include "Net/UnrealNetwork.h"
 
 void AMainGameState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
 {
